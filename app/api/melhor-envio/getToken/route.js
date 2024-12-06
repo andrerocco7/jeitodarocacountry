cffb26adf7f6025b6f269f859ba103465a26b551
Leonardo Lima Dorea <leolimadorea@gmail.com>
2024-12-06 11:04:47 -0300

    images categories

--- app/api/melhor-envio/getToken/route.js
+++ app/api/melhor-envio/getToken/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const runtime = "edge"; // Updated runtime configuration
+
 export async function POST(request) {
   try {
     // Obter o corpo da requisição
@@ -40,7 +42,3 @@ export async function POST(request) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
-
-export const config = {
-  runtime: "edge", // Otimização opcional para rotas API
-};
